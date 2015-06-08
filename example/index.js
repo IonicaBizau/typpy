74fa9cd356404192cc482185ed2095137e7a9083
Ionică Bizău <bizauionica@yahoo.com>
2015-06-08 12:12:15 +0300

    Comments

--- example/index.js
+++ example/index.js
@@ -1,6 +1,14 @@
+// Dependencies
 var Typpy = require("../lib");
 
 console.log(Typpy(0));
+// => "number"
+
 console.log(Typpy(""));
+// => "string"
+
 console.log(Typpy(null));
+// => "null"
+
 console.log(Typpy([]));
+// => "array"
